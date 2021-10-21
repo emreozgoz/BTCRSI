416ab7cdbb2458cd36e871b2de3cb7e279af7735
Emre <emreozgoz@gmail.com>
2021-10-21 15:46:41 +0300

    rsi open

--- app.js
+++ app.js
@@ -10,31 +10,36 @@ const config = {
         trustServerCertificate: true
     }
 };
- 
+const sleep = (waitTimeInMs) => new Promise(resolve => setTimeout(resolve, waitTimeInMs));
+const crontime = (waitTimeInMs) => new Promise(resolve => setTimeout(run, waitTimeInMs));
+
 let driver = new Builder().forBrowser("chrome").build();
+driver.get("https://www.gate.io/en/trade/BTC_USDT");
 
-driver.get('https://www.gate.io/en/trade/BTC_USDT');
+zsdzsd();
+crontime(5000);
+async function zsdzsd(){
+await sleep(10000);
+var elems = await driver.findElements(By.css("#kline_tradingView_Indicator_box > li"));
+elems[6].click();
+}
 
-driver.sleep(20000);
-
-
-driver.findElement(By.xpath("//*[@id=\"kline_tradingView_Indicator_box\"]/li[7]")).click();
 
 
 // 0 00 06 * * ?
- cron.schedule('*/1 * * * * * ', async () => {
-     await migrate();
- });
-// // migrate();
- async function migrate() {
-     try {
-        const label = await driver.findElement(By.xpath('//*[@id="currPrice"]')).getText();
-     //   const label2 = await driver.findElement(By.xpath('/html/body/div[1]/div[1]/div/div[2]/table/tbody/tr[5]/td[2]/div/div[3]/div/div/span[1]/span')).getText();
-       // JSON.parse(label);
-        console.log(label);
-      //  console.log(label2);
+async function run (){
 
-     } catch (err) {
-         console.log(err);
-     }
- }
+    cron.schedule('*/1 * * * * * ', async () => {
+        await migrate();
+    });
+}
+   async function migrate() {
+       try {
+          let label = await driver.findElement(By.xpath('//*[@id="currPrice"]')).getText();
+          let label2 = await driver.findElement(By.xpath('')).getText();
+          console.log(`${label} ${label2}`);
+
+       } catch (err) {
+           console.log(err);
+       }
+   }
